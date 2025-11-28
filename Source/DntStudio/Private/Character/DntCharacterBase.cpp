545c3d59fd9f1af597886015ae0a7efcd4dc3fa1
xiaoxvan <xiaoxvan@example.com>
2025-11-29 00:09:30 +0800

    1

--- Source/DntStudio/Private/Character/DntCharacterBase.cpp
+++ Source/DntStudio/Private/Character/DntCharacterBase.cpp
@@ -7,6 +7,9 @@ ADntCharacterBase::ADntCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon ->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon ->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ADntCharacterBase::BeginPlay()
